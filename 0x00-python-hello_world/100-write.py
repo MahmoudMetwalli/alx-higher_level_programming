0482726a1b2f0ee3854b83bd98ca178ff0ce86e3
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-02 02:47:30 +0200

    python

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 57)
-exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
