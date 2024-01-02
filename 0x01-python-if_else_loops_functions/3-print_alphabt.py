f055afac9af5d5c4787274dcf7590373101d6295
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-03 00:26:52 +0200

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if i == 'e' or i == 'q':
-        continue
-    print("{:c}".format(i), end="")
+    if (i != 101) and (i != 113):
+        print("{:c}".format(i), end="")
