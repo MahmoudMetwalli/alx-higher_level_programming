1862958b775d719d18f3a2bd27af61e20f0c9820
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-03 00:16:11 +0200

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
+i = 61
+while (i != 173):
     if (i == 65) or (i == 71):
         continue
     else:
         print("{:c}".format(i), end="")
+    i += 1
