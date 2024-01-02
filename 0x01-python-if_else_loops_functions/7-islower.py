e20a84a13f72d12ee1ed2f0b25a66fcef666d2c1
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-03 01:08:57 +0200

    python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@
 def islower(c):
     i = ord(c)
     if (i >= 65) and (i <= 90):
-        print("f{c} is upper")
+        print(f"{c} is upper")
     else:
-        print("f{c} is lower")
+        print(f"{c} is lower")
