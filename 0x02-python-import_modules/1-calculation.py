dee58a14ad71b15a27dd7541c593e43693f64d10
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-04 21:51:20 +0200

    2024-01-04 09:51:20 PM

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as cals
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, cals.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, cals.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, cals.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, cals.div(a, b)))
