5df019f788128d2ac8470ab292130264cd31a651
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-04 22:05:56 +0200

    2024-01-04 10:05:56 PM

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv)
+    a -= 1
+    if a == 1:
+        b = "argument"
+    else:
+        b = "arguments"
+    if a == 0:
+        c = "."
+    else:
+        c = ":"
+    print("{:d} {} {}".format(a, b, c))
+    for i in range(1, (a + 1)):
+        print("{}: {}".format(i, sys.argv[i]))
