8214ac2c91d4dfb5c65086c44775db5cee8248ec
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-04 22:20:44 +0200

    2024-01-04 10:20:44 PM

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    a = len(sys.argv)
+    a -= 1
+    b = 0
+    for i in range(1, (a + 1)):
+        b += int(sys.argv[i])
+    print(f"{b}")
+    
