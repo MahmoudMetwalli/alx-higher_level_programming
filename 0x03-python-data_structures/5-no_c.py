35d2578b914d4995844cbd4ed11c3d6bd847cc94
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-07 01:24:20 +0200

    2024-01-07 01:24:20 AM

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     word = ""
-    for ele in my_string[:]:
-        if ele != 'c' and ele != 'C':
-            word += ele
+    for ele in my_string:
+        if ele == 'c' or ele == 'C':
+            continue
+        word += ele
     return word
