7d325da74115413aecafe3f369f9edd316a99dc8
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-09 01:18:55 +0200

    2024-01-09 01:18:55 AM

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+int main()
+{
+	Py_Initialize();
+
+	PyObject *myString = Py_BuildValue("s", "Hello from C!");
+
+	if (myString != NULL)
+	{
+		PyObject_Print(myString, stdout, 0);
+		printf("\n");
+
+		Py_DECREF(myString);
+	}
+	else
+	{
+		PyErr_Print();
+	}
+
+	Py_Finalize();
+
+	return (0);
+}
