9013ae8bd290229956acf37887e88ab98f87b222
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-11 03:47:38 +0200

    2024-01-11 03:47:38 AM

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     x = 0
-    if not isinstance(roman_string, str) or not roman_string:
-        return 0
+    if not (isinstance(roman_string, str)) or not (roman_string):
+        return x
     for i in roman_string:
         for j, k in romans.items():
             if i == j:
