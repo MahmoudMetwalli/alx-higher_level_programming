7e1d68d6d2fcb69f84171dcf87118cb7e5237e9b
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-11 02:19:54 +0200

    2024-01-11 02:19:54 AM

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,12 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    l = 0
+    flag = 0
     for i in a_dictionary:
-        if l == 0:
+        if flag == 0:
             k = i
             m = a_dictionary[i]
-            l = 1
+            flag = 1
         if a_dictionary[i] > m:
             k = i
             m = a_dictionary[i]
