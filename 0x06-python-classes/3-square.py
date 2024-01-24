7f04260c8e5e86b9b9fb5fff8a5cda60b875276e
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-24 03:35:53 +0200

    2024-01-24 03:35:53 AM

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-    """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
     def area(self):
         return self.__size**2
