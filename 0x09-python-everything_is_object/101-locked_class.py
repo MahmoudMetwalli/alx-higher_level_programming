9d80458a2fa86755e925656389e9d50803308d2b
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-30 22:53:31 +0200

    2024-01-30 10:53:31 PM

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 """to be locked"""
 
+
 class LockedClass:
-    __slots__ = ['first_name']
+
+    def __init__(self):
+        """to be locked"""
+        self.__dict__
+
+    def __setattr__(self, attr, value):
+        if attr != "first_name":
+            raise AttributeError(f"'LockedClass' object has no attribute '{attr}'")
+        else:
+            self.__dict__[attr] = value
