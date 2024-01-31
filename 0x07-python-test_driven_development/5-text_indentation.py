821f1c5a7b8ddd3cc3155963ea90886db7650e0d
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-31 02:17:52 +0200

    2024-01-31 02:17:52 AM

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 
 
 def text_indentation(text):
-    """function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """prints a 2 new lines after each of these characters: ., ? and :"""
     if type(text) != str:
         raise TypeError("text must be a string")
     for t in ".?:":
