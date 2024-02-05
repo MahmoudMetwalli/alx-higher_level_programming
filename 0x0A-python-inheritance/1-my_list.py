668734b36ab193178ab09b7cf41c27a86e8ad99a
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-05 21:13:00 +0200

    2024-02-05 09:13:00 PM

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
-        try:
-            print(sorted(self))
-        except Exception:
-            pass
+
+        print(sorted(list(self)))
