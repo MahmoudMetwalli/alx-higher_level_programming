fe7e5f8822b2a053f8559d5ff26a2bfbcd475a1a
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-05 22:52:56 +0200

    2024-02-05 10:52:56 PM

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""checks object"""
+
+def inherits_from(obj, a_class):
+    """checks object"""
+    if isinstance((obj), a_class) and not type(obj) == a_class:
+        return True
+    else:
+        return False
