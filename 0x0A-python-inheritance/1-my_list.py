912125d2ac73aa9010fa19f3420c5517c33bfda5
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-05 21:24:46 +0200

    2024-02-05 09:24:46 PM

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,10 @@
 
 class MyList(list):
     """Mylist as list"""
-    pass
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
-
+        for i in list(self):
+            if not isinstance(i, int):
+                raise TypeError("unorderable types: NoneType() < int()")
         print(sorted(self))
