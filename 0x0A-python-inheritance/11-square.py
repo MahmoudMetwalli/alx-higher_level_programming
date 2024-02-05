018b9f78fd756ea4e4ec094fe962bbc1bacff8ae
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-06 00:38:43 +0200

    2024-02-06 12:38:43 AM

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -48,4 +48,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """print"""
-        return f"[Rectangle] {self.__size}/{self.__size}"
+        return f"[Square] {self.__size}/{self.__size}"
