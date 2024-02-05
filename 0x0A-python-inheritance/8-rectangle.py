851a7ecb3bfb9bf3911f6b5f28ef93434d008a87
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-06 00:07:31 +0200

    2024-02-06 12:07:31 AM

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,7 +15,7 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
+
 
 class Rectangle(BaseGeometry):
     """an empty class Rectangle"""
