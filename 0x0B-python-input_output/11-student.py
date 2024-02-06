e8d36afa11bc308ca10a70288c5b867e74adf3f1
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-07 01:23:21 +0200

    2024-02-07 01:23:21 AM

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""defines a student"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initsss"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        diction = {}
+        if isinstance(attrs, list):
+            for i in attrs:
+                for key, value in vars(self).items():
+                    if i == key:
+                        diction[i] = value
+            return diction
+        return vars(self)
+
+    def reload_from_json(self, json):
+        """changes attributes"""
+        for key, value in json.items():
+            self.__dict__[key] = value
