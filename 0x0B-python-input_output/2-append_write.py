b118cd4925769814db3c698348f7a554d2055792
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-06 20:20:53 +0200

    2024-02-06 08:20:53 PM

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""appending"""
+
+
+def append_write(filename="", text=""):
+    """appending"""
+    with open(filename, mode= 'a', encoding= 'utf-8') as a_file:
+        num_wrote = a_file.write(text)
+    return num_wrote
