41b55628652e749f7f7c458c8a2944417a9dbd51
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-07 18:36:25 +0200

    2024-02-07 06:36:25 PM

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
+"""reads stdin line by line and computes metrics"""
 import sys
-import io
+import signal
 
-#input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-#with open(input, "r", encoding="utf-8") as file:
- #   for line in file:
-  #      print(line)
 
-#input = io.TextIOWrapper(sys.stdin , encoding='utf-8')
+def handler(signum, frame):
+    """handles ctrl c"""
+    print(f"File size: {TOTAL_FILE_SIZE}")
+    for key2, value2 in codes_dictionary.items():
+        if value2 == 0:
+            print("", end="")
+        else:
+            print(f"{key2}: {value2}")
 
-dictstatus = {}
-totalsize = 0
-totalcount = 0
+signal.signal(signal.SIGINT, handler)
+
+codes_list = ["200", "301", "400", "401", "403", "404", "405", "500"]
+codes_dictionary = {"200": 0, "301": 0, "400": 0,
+                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+COUNT = 0
+TOTAL_FILE_SIZE = 0
 for line in sys.stdin:
-    split = line.split()
-    status = split[-2]
-    totalsize += int(split[-1])
-    if status in dictstatus.keys():
-        dictstatus[status] += 1
-    else:
-        dictstatus[status] = 1
-    totalcount += 1
-    if totalcount == 10:
-        sortme = sorted(dictstatus.keys())
-        print("File size:", totalsize)
-        for keys in sortme:
-            print("{}: {}".format(keys, dictstatus[keys]))
-        totalcount = 0
-        continue
+    if (line.split())[7] in codes_list:
+        codes_dictionary[(line.split())[7]] += 1
+    TOTAL_FILE_SIZE += int((line.split())[8])
+    COUNT += 1
+    if COUNT == 10:
+        print(f"File size: {TOTAL_FILE_SIZE}")
+        for key, value in codes_dictionary.items():
+            if value == 0:
+                print("", end="")
+            else:
+                print(f"{key}: {value}")
+        COUNT = 0
