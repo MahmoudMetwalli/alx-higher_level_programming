85269b3869983dd410985afa88a0034a8a461c34
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-07 02:19:57 +0200

    2024-02-07 02:19:57 AM

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,19 +4,20 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """append after"""
-    length = len(search_string)
+    if len(search_string) == 0 or len(new_string) == 0:
+        return
     total = ""
     with open(filename, mode='r+', encoding='utf-8') as a_file:
         for line in a_file.readlines():
             flag_2 = 0
-            for compare in range(0,len(line)):
+            for compare in range(0, len(line)):
                 flag = 0
                 if line[compare] == search_string[0]:
-                    for i in range(0, length):
+                    for i in range(0, len(search_string)):
                         if line[compare] == search_string[i]:
                             flag += 1
                         compare += 1
-                if flag == length:
+                if flag == len(search_string):
                     flag_2 = 1
             total += line
             if flag_2:
