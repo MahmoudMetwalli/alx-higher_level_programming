d20809765ff9716dde5e430a1161c22049719e37
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-07 22:37:05 +0200

    2024-02-07 10:37:05 PM

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,7 +5,7 @@ import sys
 codes_list = ['200', '301', '400', '401', '403', '404', '405', '500']
 codes_dictionary = {'200': 0, '301': 0, '400': 0,
                     '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-COUNT = 0
+COUNT = 1
 TOTAL_FILE_SIZE = 0
 try:
     for line in sys.stdin:
@@ -24,12 +24,11 @@ try:
             for key, value in codes_dictionary.items():
                 if value:
                     print(f"{key}: {value}")
-            COUNT = 0
-    if COUNT == 10:
-        print(f"File size: {TOTAL_FILE_SIZE}")
-        for key, value in codes_dictionary.items():
-            if value:
-                print(f"{key}: {value}")
+            COUNT = 1
+    #print(f"File size: {TOTAL_FILE_SIZE}")
+    #for key, value in codes_dictionary.items():
+        #if value:
+            #print(f"{key}: {value}")
 except KeyboardInterrupt:
     print(f"File size: {TOTAL_FILE_SIZE}")
     for key, value in codes_dictionary.items():
