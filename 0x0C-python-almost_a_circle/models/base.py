4f2ea529b5a9767c63342cc2b7cba76df692bad8
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-10 23:35:08 +0200

    2024-02-10 11:35:08 PM

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,15 +31,41 @@ class Base:
     def save_to_file(cls, list_objs):
         """writes JSON represntation to a file"""
         file_name = cls.__name__ + ".json"
-        print(file_name)
         dict_list = []
         for instance in list_objs:
             dict_list.append(instance.to_dictionary())
         with open(file_name, 'w', encoding='utf-8') as file_a:
             file_a.write(Base.to_json_string(dict_list))
-        print("iam here")
 
     @staticmethod
     def from_json_string(json_string):
         """loads an object form JSON"""
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        instance = None
+        if cls is Rectangle:
+            instance = Rectangle(1,1)
+        if cls is Square:
+            instance = Square(1)
+        instance.update(**dictionary)
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """loads JSON from file and convert it to object"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        file_name = cls.__name__ + ".json"
+        if cls is Rectangle:
+            with open(file_name, encoding='utf-8') as file_a:
+                return [Rectangle.create(**dictionary) for dictionary
+                        in Rectangle.from_json_string(file_a.read())]
+        if cls is Square:
+            with open(file_name, encoding='utf-8') as file_a:
+                return [Square.create(**dictionary) for dictionary
+                        in Square.from_json_string(file_a.read())]
