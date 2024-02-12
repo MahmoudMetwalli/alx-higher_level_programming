75d041af9e033fbff16519cb330b806705941439
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-12 23:52:41 +0200

    2024-02-12 11:52:41 PM

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -48,5 +48,27 @@ class TestOfBase(unittest.TestCase):
         self.assertEqual(Base.to_json_string(case_b), "[]")
         self.assertEqual(Base.to_json_string(case_c), "[]")
 
+    def test_4_save_to_file(self):
+        """test for save_to_file method"""
+        case_r1_a = Rectangle(10, 7, 2, 8)
+        case_r2_a = Rectangle(2, 4)
+        case_a = [case_r1_a, case_r2_a]
+        case_b = []
+        case_c = None
+        Rectangle.save_to_file(case_a)
+        with open("Rectangle.json", encoding='utf-8') as file_case_a:
+            self.assertEqual(len(file_case_a.read()), len('[{"y": 8,\
+ "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2\
+, "width": 2, "height": 4}]'))
+        Square.save_to_file(case_b)
+        with open("Square.json", encoding='utf-8') as file_case_b:
+            self.assertEqual(len(file_case_b.read()), len("[]"))
+        Square.save_to_file(case_c)
+        with open("Square.json", encoding='utf-8') as file_case_c:
+            self.assertEqual(len(file_case_c.read()), len("[]"))
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", encoding='utf-8') as file_case_d:
+            self.assertEqual(len(file_case_d.read()), len("[]"))
+
 if __name__ == "__main__":
     unittest.main()
