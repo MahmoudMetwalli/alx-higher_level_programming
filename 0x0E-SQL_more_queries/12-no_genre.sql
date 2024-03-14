09b816bf90f086e8822cca20d631f39eae6e70d2
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-14 03:46:41 +0200

    2024-03-14 03:46:41 AM

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
-        LEFT OUTER JOIN `tv_show_genres` AS g
+        LEFT JOIN `tv_show_genres` AS g
 	ON s.`id` = g.`show_id`
 	WHERE g.'genre_id' IS NULL
  ORDER BY s.`title`, g.`genre_id`;
