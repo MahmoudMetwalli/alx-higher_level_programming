53dc09ba9746f22f2cb0622e3b6624b8863c3a0e
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-14 03:29:35 +0200

    2024-03-14 03:29:35 AM

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- Lists all cities contained in the database
-SELECT cities.id, cities.name, states.name ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+	FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
