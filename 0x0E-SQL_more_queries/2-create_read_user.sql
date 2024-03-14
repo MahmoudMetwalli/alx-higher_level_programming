a944970c3cae05b035107a4e6e62210ebd104643
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-14 02:12:56 +0200

    2024-03-14 02:12:56 AM

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grants privilges to user
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
