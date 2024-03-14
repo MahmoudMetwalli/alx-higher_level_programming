409030e52e470e262a41b38c9a772aa77c290565
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-14 03:05:53 +0200

    2024-03-14 03:05:53 AM

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- Creates database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(cities.id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES(hbtn_0d_usa.cities.id));
