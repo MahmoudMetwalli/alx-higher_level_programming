3bbb1466dbe829fe47443d6606ef424c4b8d6670
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-09 04:04:19 +0200

    2024-04-09 04:04:19 AM

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
-// computes and prints a factorial
+// Computes and prints a factorial recursively
 
 function factorial (n) {
-	if ((isNaN(n)) || (n === 1)) {
-	  return 1;
-	} else {
-	  return n * factorial(n - 1);
+	if (n === 1) {
+	  return (1);
 	}
+	return (n * factorial(n - 1));
   }
   
-  console.log(factorial(parseInt(process.argv[2])));
+  const args = process.argv;
+  
+  if (isNaN(args[2])) {
+	console.log('1');
+  } else {
+	let num = factorial(parseInt(args[2], 10));
+	console.log(num);
+  }
