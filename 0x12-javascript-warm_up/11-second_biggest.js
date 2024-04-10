a83567a650b67ab44dc46d0303c09611830317e3
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-10 03:16:15 +0200

    2024-04-10 03:16:15 AM

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -13,11 +13,19 @@ if (converted.length === 0 || converted.length === 1) {
 } else {
   i = 1;
   let target = converted[0];
-  let subtarget = converted[1];
+  let subtarget = converted[0];
   while (i < converted.length) {
     if (target < converted[i]) {
-      subtarget = target;
       target = converted[i];
+    }
+    i += 1;
+  }
+  i = 1;
+  while (i < converted.length) {
+    if (target === converted[i]) {
+      subtarget = subtarget;
+    } else if (subtarget < converted[i]) {
+      subtarget = converted[i];
     }
     i += 1;
   }
