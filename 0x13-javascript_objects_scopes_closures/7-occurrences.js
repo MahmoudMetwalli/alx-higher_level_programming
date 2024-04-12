62244291132ab41b6f312493a92aff8cbbdf0018
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-12 20:31:38 +0200

    2024-04-12 08:31:38 PM

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  let num = 0;
+  function number () {
+    for (let i = 0; i < list.length; i++) {
+	if (list[i] === searchElement) {
+	  num += 1;
+	}
+    }
+	return num;
+  }
+  return number();
 };
