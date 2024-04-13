31b778a5d321888d85c2bbba1761294d3e145124
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-13 13:58:18 +0200

    2024-04-13 01:58:18 PM

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
-exports.dict = {
-	89: 1,
-	90: 2,
-	91: 1,
-	92: 3,
-	93: 1,
-	94: 2
-  };
+const dic = require('./101-data').dict;
+const vallst = Object.values(dic);
+const kylst = Object.keys(dic);
+const valnd = [...new Set(vallst)];
+const finaldic = {};
+for (let i = 0; i < valnd.length; i++) {
+  const templst = [];
+  for (let j = 0; j < kylst.length; j++) {
+    if (dic[kylst[j]] === valnd[i]) {
+      templst.push(kylst[j]);
+    }
+  }
+  finaldic[valnd[i]] = templst;
+}
+console.log(finaldic);
