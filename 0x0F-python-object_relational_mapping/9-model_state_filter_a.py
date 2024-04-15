2296cd1d0ef2e807663e82ed7d5f0d6e383de44b
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-16 01:24:45 +0200

    2024-04-16 01:24:45 AM

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""to get all states"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.
+                                  argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State.id,
+                           State.name).filter(
+                               State.name.like('%a%')).order_by(State.id)
+    if not result:
+        print("Nothing")
+    else:
+        for id, name in result:
+            print("{}: {}".format(id, name))
