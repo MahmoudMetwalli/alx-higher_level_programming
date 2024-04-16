d439ac7a4bda02081824b1b8d49fa470e5ddf61a
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-16 04:43:56 +0200

    2024-04-16 04:43:56 AM

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,10 +2,8 @@
 """Lists states"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
 from sqlalchemy.orm import relationship, backref
-
-Base = declarative_base()
 
 
 class State(Base):
