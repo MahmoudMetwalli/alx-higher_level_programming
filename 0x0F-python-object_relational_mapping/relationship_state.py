cdf79737689963949515de0b81a882501418efcc
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-16 03:29:05 +0200

    2024-04-16 03:29:05 AM

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""To make State object"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import City
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class inheriting from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
