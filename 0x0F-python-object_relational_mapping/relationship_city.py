95077d1d8a0c545dbcbd587121db0ae5d5098c06
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-16 04:45:16 +0200

    2024-04-16 04:45:16 AM

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """To make City object"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from relationship_state import Base
 
-
+Base = declarative_base()
 
 class City(Base):
     """City class inheriting from Base"""
