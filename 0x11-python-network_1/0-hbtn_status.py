58ac95e20adbf28bbdf7f8844803ed0c6b345f2b
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 22:07:17 +0300

    2024-04-27 10:07:17 PM

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         html = resp.read()
         print("Body response:\n\t- type: {}\
-              \n\t- content: {}\
-              \n\t- utf8 content: {}\
-              ".format(type(html), html, html.decode("utf-8")))
+\n\t- content: {}\
+\n\t- utf8 content: {}\
+".format(type(html), html, html.decode("utf-8")))
