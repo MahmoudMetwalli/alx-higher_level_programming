41e33ddcfd154bfa4343cdd90b265ba380302041
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 05:56:23 +0300

    2024-04-27 05:56:23 AM

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends JSON POST as the first arg,& displays body
-curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2"
+curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
