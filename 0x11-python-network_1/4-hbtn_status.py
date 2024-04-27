d1d62f8df3c4169726c03eaa9c56ebbcf57a150f
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 23:00:43 +0300

    2024-04-27 11:00:43 PM

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Status of connection"""
+import requests
+
+
+
+if __name__ == "__main__":
+    URL = 'https://alx-intranet.hbtn.io/status'
+    req = requests.get(URL)
+    html = req.text
+    print("Body response:\n\t- type: {}\
+\n\t- content: {}\
+".format(type(html), html))
