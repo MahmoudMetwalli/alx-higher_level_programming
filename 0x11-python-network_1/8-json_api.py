2976a86ae3793618a24ba3c9ffe8b9414dd756bc
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 23:58:21 +0300

    2024-04-27 11:58:21 PM

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Status of connection"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    values = {'q': q}
+    URL = 'http://0.0.0.0:5000/search_user'
+    try:
+        response = requests.post(URL, data=values)
+        try:
+            html = response.json()
+            if len(html) == 0:
+                print('No result')
+            else:
+                print("[{}] {}".format(html['id'], html['name']))
+        except requests.exceptions.InvalidJSONError:
+            print('Not a valid JSON')
+    except Exception:
+        pass
