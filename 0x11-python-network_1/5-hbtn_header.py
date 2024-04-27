3754dc1a1c36bd2abb191d551700a99be8f9b293
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 23:06:01 +0300

    2024-04-27 11:06:01 PM

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,6 +6,6 @@ import sys
 
 if __name__ == "__main__":
     URL = sys.argv[1]
-    req = requests.get(URL)
+    req = requests.get(URL, timeout=1)
     html = req.headers
     print(html['X-Request-Id'])
