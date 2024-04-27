edbe4a9b307b21546a9e92ff8827795034568749
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 21:57:46 +0300

    2024-04-27 09:57:46 PM

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn\
-                                .io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
         print("Body response:\n\t- type: {}\
               \n\t- content: {}\
               \n\t- utf8 content: {}\
