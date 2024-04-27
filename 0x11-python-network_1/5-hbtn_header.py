0f5b011ed545fdd24df853c94e10ffe5f458413b
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 23:09:33 +0300

    2024-04-27 11:09:33 PM

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,10 @@ import sys
 
 if __name__ == "__main__":
     URL = sys.argv[1]
-    req = requests.get(URL, timeout=1)
-    if req is not None:
-        html = req.headers
-        print(html['X-Request-Id'])
+    try:
+        req = requests.get(URL, timeout=1)
+        if req is not None:
+            html = req.headers
+            print(html['X-Request-Id'])
+    except Exception:
+        pass
