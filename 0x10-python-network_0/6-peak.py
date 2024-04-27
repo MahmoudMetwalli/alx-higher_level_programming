ecb57220faee6cda8770f1e5f94dadb8387ac9a4
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 14:16:42 +0300

    2024-04-27 02:16:42 PM

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,19 +3,10 @@
 
 
 def find_peak(list_of_integers):
-    """Finding peaks"""
-    if len(list_of_integers) == 0:
-        return None
-    return max(list_of_integers)
-"""
-    if len(list_of_integers) == 1 or list_of_integers[0] > list_of_integers[1]:
-        return list_of_integers[0]
-    if list_of_integers[0] > list_of_integers[1]:
-        return list_of_integers[0]
-    for i in range(1,len(list_of_integers), 2):
-        try:
-            if list_of_integers[i] >
-        except IndexError:
-            pass
-"""
-
+    """BRUTE force implementation
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
