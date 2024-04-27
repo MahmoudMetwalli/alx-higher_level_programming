02da4a203d066053ccc3e07f6213ed211ad5a731
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-27 05:37:38 +0300

    2024-04-27 05:37:38 AM

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 """To Find Peaks"""
 
-def divide(array, low, high):
-    """divide and conquer"""
-
-    mid = int((high + low)/2)
-    if array[mid-1] <= array[mid] >= array[mid+1]:
-        return array[mid]
-    elif array[mid] > array[mid+1]:
-        return divide(array, low, mid-1)
-    elif array[mid] < array[mid+1]:
-        return divide(array, mid+1, high)
-
-
 def find_peak(list_of_integers):
-    """Find peak of a list"""
-
-    if not list_of_integers:
-        return None
-    return divide(list_of_integers, 0, len(list_of_integers)-1)
+    """BRUTE force implementation
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
