863916b7907a0f5fad3381dc109ece7a6153283d
MahmoudMetwalli <mammkbih@gmail.com>
2024-04-28 22:56:38 +0300

    2024-04-28 10:56:38 PM

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Status of connection"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    try:
+        req = requests.get('https://api.github.com\
+/repos/{}/{}/commits'.format(owner, repo), timeout=1)
+        html = req.json()
+        for i in range(0, 10):
+            print(html[i]['sha'], end=" ")
+            print(html[i]['commit']['author']['name'])
+    except Exception as err:
+        print('failed')
+        print(err)
