1bf76a45f7ec36b203899158faa383352bbc6192
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 19:36:42 +0300

    2024-05-22 07:36:42 PM

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
+const request = require('request');
+const util = require('util');
 const args = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + args;
-const request = require('request');
-request(url, (err, response, body) => {
-  if (err) {
+
+// Convert the request function to a Promise-based one
+const requestPromise = util.promisify(request);
+
+async function getCharacterNames() {
+  try {
+    const response = await requestPromise(url);
+    const characters = JSON.parse(response.body).characters;
+    for (const character of characters) {
+      const characterResponse = await requestPromise(character);
+      console.log(JSON.parse(characterResponse.body).name);
+    }
+  } catch (err) {
     console.error(err);
-    return;
   }
-  const characters = JSON.parse(body).characters;
-  characters.forEach(async (character) => {
-    request(character, (err, response, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
-});
+}
+
+getCharacterNames();
