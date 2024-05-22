2c31377aa2d2dfcdf88a87d1e303a61fc2702119
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 18:17:36 +0300

    2024-05-22 06:17:36 PM

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  fs.writeFile(path, body, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
