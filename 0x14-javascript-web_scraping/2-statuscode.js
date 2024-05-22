16bec13d2b1570d1cb8bd3eac9175141dea14a20
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 14:26:31 +0300

    2024-05-22 02:26:31 PM

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,8 @@ const args = process.argv[2];
 const request = require('request');
 request(args, (err, response) => {
   if (err) {
-  console.error(err);
-  return;}
+    console.error(err);
+    return;
+  }
   console.log('Code: ', response.statusCode); // Print the response status code if a response was received
 });
