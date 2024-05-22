a7a3a104d08f4f8d2ebc15487a82d92f59eaf680
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 18:06:25 +0300

    2024-05-22 06:06:25 PM

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-const url = 'https://swapi-api.alx-tools.com/api/people/18/';
 const request = require('request');
+const url = process.argv[2];
 request(url, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(JSON.parse(body).films.length);
+  const films = JSON.parse(body).results;
+  console.log(films.reduce((count, movie) => {
+    if (movie.characters.find((character) => character.endsWith('/18/'))) {
+      count += 1;
+    }
+    return count;
+  }, 0));
 });
