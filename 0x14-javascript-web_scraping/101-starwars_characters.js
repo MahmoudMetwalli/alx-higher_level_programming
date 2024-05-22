92270adf1b0f8132920323a8c5925b1d4c7e7474
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 19:37:39 +0300

    2024-05-22 07:37:39 PM

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,7 +7,7 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + args;
 // Convert the request function to a Promise-based one
 const requestPromise = util.promisify(request);
 
-async function getCharacterNames() {
+async function getCharacterNames () {
   try {
     const response = await requestPromise(url);
     const characters = JSON.parse(response.body).characters;
